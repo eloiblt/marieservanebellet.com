c713e84dafe1265a84b19c35e76dee6ac086a571
Eloi Bellet <eloi.bellet@gmail.com>
2020-04-09 14:11:53 +0200

    bugfix cors

--- api/functions/src/config/constants.ts
+++ api/functions/src/config/constants.ts
@@ -1,5 +1,5 @@
 export const dataBaseUrl = 'https://marieservanebellet-api.firebaseio.com';
 export const corsOptions = {
-  origin: 'https://marieservanebellet.com/',
+  origin: 'https://marieservanebellet.com',
   optionsSuccessStatus: 200
 };
