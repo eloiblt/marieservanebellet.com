5266e7685c3028bc13629679b3faf99104f13954
Eloi Bellet <eloi.bellet@gmail.com>
2020-04-15 16:21:54 +0200

    add secret files

---
+++ api/src/config/constants.ts
@@ -0,0 +1,3 @@
+export const frontUrl = 'https://marieservanebellet.com';
+export const dataBaseUrl = 'https://marieservanebellet-api.firebaseio.com';
+export const jwtSecret = '1341K§/3/.3DSFDfsdf';
--- front/server.js
+++ front/server.js
@@ -4,9 +4,4 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname + '/dist'));
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-
 app.listen(80, () => console.log('Listening on port 80 !'));
