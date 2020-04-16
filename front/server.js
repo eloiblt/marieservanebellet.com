93e7606908b9323f32aba35c4466dd981ced6e38
Eloi Bellet <eloi.bellet@gmail.com>
2020-04-16 11:18:55 +0200

    add https

--- front/server.js
+++ front/server.js
@@ -1,7 +1,30 @@
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
+// Certificate
+const privateKey = fs.readFileSync('../../.certbot/config/live/marieservanebellet.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('../../.certbot/config/live/marieservanebellet.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('../../.certbot/config/live/marieservanebellet.com/chain.pem', 'utf8');
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+};
+
 app.use(express.static(__dirname + '/dist'));
-app.listen(80, () => console.log('Listening on port 80 !'));
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+  console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+  console.log('HTTPS Server running on port 443');
+});
