604e68e9f137761a4178a718f4ea59008bb51b0c
Eloi Bellet <eloi.bellet@gmail.com>
2020-04-18 16:14:02 +0200

    about component

--- front/src/app/modules/home/about/about.component.ts
+++ front/src/app/modules/home/about/about.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  public photoPath = '../../../../assets/pictures/photo.jpg';
+
   constructor() { }
 
   ngOnInit(): void {
