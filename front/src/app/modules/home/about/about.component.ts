6d670200596287c75558a51ff71197dd1a403f7f
Eloi Bellet <eloi.bellet@gmail.com>
2020-04-18 16:17:06 +0200

    add loader about

--- front/src/app/modules/home/about/about.component.ts
+++ front/src/app/modules/home/about/about.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   public photoPath = '../../../../assets/pictures/photo.jpg';
+  public show = false;
 
   constructor() { }
 
