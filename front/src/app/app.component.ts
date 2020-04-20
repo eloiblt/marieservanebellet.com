9f64b748eebf2b5a8cf89b0201741f5292b60678
Eloi Bellet <eloi.bellet@gmail.com>
2020-04-20 19:12:25 +0200

    add google analytics

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+declare var gtag;
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +10,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(router: Router) {
+    if (environment.production) {
+      const script = document.createElement('script');
+      script.async = true;
+      script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-164004514-1';
+      document.head.prepend(script);
+    }
+
+    const navEndEvents = router.events.pipe(
+      filter(
+        event => event instanceof NavigationEnd
+      )
+    );
+
+    navEndEvents.subscribe(
+      (event: NavigationEnd) => {
+        gtag('config', 'UA-164004514-1', {
+          page_path: event.urlAfterRedirects,
+        });
+      }
+    );
+  }
 }
