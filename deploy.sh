f449dcd52c9a1d8003ca81671a83f2d0fcd7e7eb
Eloi Blt <55507929+eloiblt@users.noreply.github.com>
2020-09-19 13:07:35 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ cd /var/www/marieservanebellet.com/marieservanebellet/api
 rm -r node_modules
 npm install
 
-cd /var/www/marieservanebellet.com/marieservanebellet/api
+cd /var/www/marieservanebellet.com/marieservanebellet/front
 rm -r node_modules
 npm install
 
@@ -18,6 +18,7 @@ cd /var/www/marieservanebellet.com/public_html
 rm -r *
 cp -r /var/www/marieservanebellet.com/marieservanebellet/front/dist/* .
 
-set NODE_ENV=production forever start ./dist/app.js
-
-
+forever stopall
+NODE_ENV=production forever start /var/www/marieservanebellet.com/marieservanebellet/api/dist/app.js
+cd /var/www/marieservanebellet.com/pictures/
+forever start ftp.js
