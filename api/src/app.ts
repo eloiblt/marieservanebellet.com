9a205d2e416680b1fbe0d8d2280128fc42c94c13
Eloi Bellet <eloi.bellet@gmail.com>
2020-09-19 11:57:01 +0200

    update https for back

--- api/src/app.ts
+++ api/src/app.ts
@@ -51,16 +51,28 @@ if (process.env.NODE_ENV === "development") {
     optionsSuccessStatus: 200
   })); // allow front only
 
-  const httpServer = http.createServer(app);
-  httpServer.listen(3000, () => {
-    console.log('HTTP Server is listening on 3000');
-  });
+  // Certificate
+  const privateKey = fs.readFileSync('/etc/letsencrypt/live/marieservanebellet.com/privkey.pem', 'utf8');
+  const certificate = fs.readFileSync('/etc/letsencrypt/live/marieservanebellet.com/cert.pem', 'utf8');
+  const chain = fs.readFileSync('/etc/letsencrypt/live/marieservanebellet.com/chain.pem', 'utf8');
 
-  app.use('/login', loginRouter);
-  app.use('/pictures', picturesRouter);
-  app.use('/categoryPictures', categoryPicturesRouter);
-  app.use('/contact', contactRouter);
+  const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: chain
+  };
 
-  app.get('/status', (req, res) => {
-    res.send('API ON');
+  const httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(3000, () => {
+    console.log('HTTPS Server is listening on 3000');
   });
+}
+
+app.use('/login', loginRouter);
+app.use('/pictures', picturesRouter);
+app.use('/categoryPictures', categoryPicturesRouter);
+app.use('/contact', contactRouter);
+
+app.get('/status', (req, res) => {
+  res.send('API ON');
+});
