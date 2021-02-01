092ec0b9cff5775822db9ac647f38435fe1f0bf9
Eloi Bellet <eloi.bellet@gmail.com>
2021-02-01 09:18:33 +0100

    angular updated to 11

--- front/src/app/modules/home/pictures/pictures.component.spec.ts
+++ front/src/app/modules/home/pictures/pictures.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PicturesComponent } from './pictures.component';
 
@@ -6,7 +6,7 @@ describe('PicturesComponent', () => {
   let component: PicturesComponent;
   let fixture: ComponentFixture<PicturesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PicturesComponent ]
     })
