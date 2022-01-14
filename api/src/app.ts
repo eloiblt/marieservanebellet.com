42ef90627f2597fd21ae4306c592711d667f4305
eloiblt <eloi.bellet@apollossc.com>
2022-01-14 23:30:48 +0100

    dockerisation v1

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,18 +2,16 @@ import cors from 'cors';
 import * as dotenv from "dotenv";
 
 if (process.env.NODE_ENV === 'production') {
-  dotenv.config({ path: `/home/eloiblt/marieservanebellet/api/.env.production` });
-} else if (process.env.NODE_ENV === 'development') {
+  dotenv.config({ path: `.env.production` });
+} else {
   dotenv.config({ path: `.env.development` });
 }
 
 import express from 'express';
 import fileupload from 'express-fileupload';
 import rateLimit from 'express-rate-limit';
-import fs from 'fs';
 import helmet from 'helmet';
 import * as http from 'http';
-import * as https from 'https';
 import mongoose from 'mongoose';
 import * as constants from './config/constants';
 import categoryPicturesRouter from './routes/categoryPicturesRouter';
@@ -28,51 +26,30 @@ mongoose.connect(constants.dataBaseUrl, {
 });
 
 const db = mongoose.connection;
-
 db.on('error', () => console.log('Error during the database connection'));
 db.once('connected', () => console.log("Connected to database"));
 
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(rateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minute
-  max: 300 // limit each IP to 100 requests per windowMs
-}));
+app.use(rateLimit({ windowMs: 1 * 60 * 1000, max: 300 }));
 app.use(fileupload());
 
-if (process.env.NODE_ENV === "development") {
-  console.log('Development environnement');
-
-  app.use(cors()); // allow *
-
-  const httpServer = http.createServer(app);
-  httpServer.listen(3000, () => {
-    console.log('HTTP Server is listening on 3000');
-  });
-} else if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   console.log('Production environnement');
 
   app.use(cors({
     origin: constants.frontUrl,
     optionsSuccessStatus: 200
-  })); // allow front only
+  }));
 
-  // Certificate
-  const privateKey = fs.readFileSync('/etc/letsencrypt/live/marieservanebellet.com/privkey.pem', 'utf8');
-  const certificate = fs.readFileSync('/etc/letsencrypt/live/marieservanebellet.com/cert.pem', 'utf8');
-  const chain = fs.readFileSync('/etc/letsencrypt/live/marieservanebellet.com/chain.pem', 'utf8');
-
-  const credentials = {
-    key: privateKey,
-    cert: certificate,
-    ca: chain
-  };
-
-  const httpsServer = https.createServer(credentials, app);
-  httpsServer.listen(3000, () => {
-    console.log('HTTPS Server is listening on 3000');
+  const httpServer = http.createServer(app);
+  httpServer.listen(8080, () => {
+    console.log('HTTP Server is listening on 8080');
   });
+} else {
+  console.log('Development environnement');
+  app.use(cors());
 }
 
 app.use('/login', loginRouter);
--- renewCertbotAndRebootBack.sh
+++
@@ -1,6 +0,0 @@
-certbot renew --dry-run
-su eloiblt
-forever stopall
-NODE_ENV=production forever start /home/eloiblt/marieservanebellet/api/dist/app.js
-exit
-systemctl restart apache2
