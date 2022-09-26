380a9c6720d34ed31bf704d514058c5de768855b
Eloi Blt <eloi.bellet@gmail.com>
2022-09-26 10:49:58 +0200

    remove etsy

--- front/src/app/helpers/constants.ts
+++ front/src/app/helpers/constants.ts
@@ -1,3 +1,2 @@
-export const etsyUrl = 'https://www.etsy.com/shop/MarieServaneArt?ref=l2-about-shopname';
 export const instaUrl = 'https://www.instagram.com/marieservaneblt/';
 export const githubUrl = 'https://github.com/eloiblt';
