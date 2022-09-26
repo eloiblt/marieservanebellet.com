82c89b9e04a1ca82488a67bf91cdf2561cce80f6
Eloi Blt <eloi.bellet@gmail.com>
2022-09-26 11:18:00 +0200

    try fix sonarlint

--- api/src/routes/categoryPicturesRouter.ts
+++ api/src/routes/categoryPicturesRouter.ts
@@ -1,49 +1,63 @@
-import * as express from 'express';
-import { CategoryPictures, CategoryPicturesCollection } from '../models/model';
-import { authenticateJWT } from '../middlewares/authenticate';
+import * as express from "express";
+import { CategoryPictures, CategoryPicturesCollection } from "../models/model";
+import { authenticateJWT } from "../middlewares/authenticate";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  CategoryPicturesCollection.find().lean()
-    .then(docs => {
-      const categoryPictures: CategoryPictures[] = docs.map(d => {
+router.get("/", (req, res) => {
+  CategoryPicturesCollection.find()
+    .lean()
+    .then((docs) => {
+      const categoryPictures: CategoryPictures[] = docs.map((d) => {
         const { _id, ...categoryPicture } = d;
         return categoryPicture as CategoryPictures;
       });
       res.status(200).send(categoryPictures);
     })
-    .catch(err => res.status(500).send());
+    .catch((err) => res.status(500).send());
 });
 
-router.post('/', authenticateJWT, (req, res) => {
+router.post(
+  "/",
+  authenticateJWT,
+  body("id").isInt(),
+  body("name").isString(),
+  body("show").isBoolean(),
+  (req, res) => {
+    // const categoryPictures: CategoryPictures = {
+    //   id: req.body.id,
+    //   name: req.body.name,
+    //   show: req.body.show
+    // } as CategoryPictures;
+
+    CategoryPicturesCollection.create(req.body)
+      .then((docs) => res.status(200).send(req.body))
+      .catch((err) => res.status(500).send());
+  }
+);
+
+router.put("/:id", authenticateJWT, (req, res) => {
   const categoryPictures: CategoryPictures = {
     id: req.body.id,
     name: req.body.name,
-    show: req.body.show
+    show: req.body.show,
   } as CategoryPictures;
 
-  CategoryPicturesCollection.create(categoryPictures)
-    .then(docs => res.status(200).send(categoryPictures))
-    .catch(err => res.status(500).send());
+  CategoryPicturesCollection.updateOne(
+    { id: categoryPictures.id },
+    categoryPictures
+  )
+    .then((docs) =>
+      res.status(200).send({ id: req.params.id, ...categoryPictures })
+    )
+    .catch((err) => res.status(500).send());
 });
 
-router.put('/:id', authenticateJWT, (req, res) => {
-  const categoryPictures: CategoryPictures = {
-    id: req.body.id,
-    name: req.body.name,
-    show: req.body.show
-  } as CategoryPictures;
-
-  CategoryPicturesCollection.updateOne({ id: categoryPictures.id }, categoryPictures)
-    .then(docs => res.status(200).send({ id: req.params.id, ...categoryPictures }))
-    .catch(err => res.status(500).send());
-});
-
-router.delete('/:id', authenticateJWT, (req, res) => {
+router.delete("/:id", authenticateJWT, (req, res) => {
   CategoryPicturesCollection.deleteOne({ id: req.params.id })
-    .then(docs => res.status(200).send())
-    .catch(err => res.status(500).send());
+    .then((docs) => res.status(200).send())
+    .catch((err) => res.status(500).send());
 });
 
 export default router;
