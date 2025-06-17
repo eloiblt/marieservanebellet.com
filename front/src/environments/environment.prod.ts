302756aa012a33fd0d7b5885b419e5d46da8fa66
Eloi Bellet <eloi.bellet@gmail.com>
2025-06-17 18:45:34 +0200

    fix env

--- front/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://msb.srv848873.hstgr.cloud/api/',
-  picturesUrl: 'https://msb.srv848873.hstgr.cloud/pictures/'
-};
---
+++ front/src/environments/environment.production.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://marieservanebellet.com/api/',
+  picturesUrl: 'https://marieservanebellet.com/pictures/'
+};
