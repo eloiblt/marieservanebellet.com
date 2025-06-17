b95952bf0701519f44426a993bcbf6c08722fdc3
Eloi Bellet <eloi.bellet@gmail.com>
2025-06-17 20:00:12 +0200

    fix mails

--- api/src/modules/contact/contact.module.ts
+++ api/src/modules/contact/contact.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from '../prisma/prisma.module';
 import { ContactController } from './contact.controller';
 import { ContactService } from './contact.service';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.SMTP_HOST,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      },
+    }),
+  ],
   controllers: [ContactController],
   providers: [ContactService],
 })
--- api/src/modules/contact/contact.service.ts
+++ api/src/modules/contact/contact.service.ts
@@ -1,41 +1,25 @@
 import { HttpException, Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
 import { ContactDto } from './dto/contact.dto';
-import * as nodeMailer from 'nodemailer';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class ContactService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private readonly mailService: MailerService) {}
 
   async sendMail(contactDto: ContactDto): Promise<void> {
-    const transporter = nodeMailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
-      secure: process.env.SMTP_TLS === 'true',
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD,
-      },
-    });
-
     contactDto.message = contactDto.message
       .replace(/&#x27;/g, "'")
       .replace(/&quot;/g, '"');
 
-    const mailOptions = {
-      from: 'marieservanebellet.site@gmail.com',
-      to: process.env.MAIL_TO,
-      bcc: process.env.MAIL_BCC,
-      subject:
-        'Nouveau mail reçu de ' +
-        contactDto.name +
-        ', <' +
-        contactDto.mail +
-        '>',
-      text: contactDto.message,
-    };
-
-    if (!(await transporter.sendMail(mailOptions))) {
+    try {
+      await this.mailService.sendMail({
+        from: 'marieservanebellet.site@gmail.com',
+        to: process.env.MAIL_TO,
+        bcc: process.env.MAIL_BCC,
+        subject: `Nouveau mail reçu de ${contactDto.name}, <${contactDto.mail}>`,
+        text: contactDto.message,
+      });
+    } catch (err) {
       throw new HttpException('Mail not sent', 500);
     }
   }
