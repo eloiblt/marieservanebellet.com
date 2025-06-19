8b1a45ccca662c99c3d78940c685c71943851087
Eloi Bellet <eloi.bellet@ext.adeo.com>
2025-06-19 15:15:37 +0200

    add install button pwa

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import { NgClass } from '@angular/common';
+import { isPlatformBrowser, NgClass } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faCircleDown } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -11,9 +12,67 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./navbar.component.scss'],
   imports: [FaIconComponent, NgClass, RouterLink],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   public showMenu = false;
   public show = false;
   public environment = environment;
   faBars = faBars;
+  faCircleDown = faCircleDown;
+
+  private readonly platformId = inject(PLATFORM_ID);
+  private deferredPrompt: any = null;
+  private isApple = false;
+  private isMobileOrTablet = false;
+
+  get canInstall(): boolean {
+    return this.isMobileOrTablet && (this.deferredPrompt || this.isApple);
+  }
+
+  ngOnInit() {
+    this.isApple =
+      /iphone|ipad|ipod/.test(navigator.userAgent.toLowerCase()) ||
+      (navigator.userAgent.includes('Mac') && 'ontouchend' in document);
+
+    this.isMobileOrTablet = this.isMobileOrTabletFunc();
+
+    if (isPlatformBrowser(this.platformId)) {
+      window.addEventListener('beforeinstallprompt', (e) => {
+        e.preventDefault();
+        this.deferredPrompt = e;
+      });
+
+      window.addEventListener('appinstalled', () => {
+        console.log('✅ L’application a été installée');
+        this.deferredPrompt = null;
+      });
+    }
+  }
+
+  installPwa() {
+    if (isPlatformBrowser(this.platformId)) {
+      if (this.isApple) {
+        window.alert('Installez l’app via “Partager > Sur l’écran d’accueil”');
+        return;
+      }
+
+      this.deferredPrompt.prompt();
+      this.deferredPrompt = null;
+    }
+  }
+
+  isMobileOrTabletFunc() {
+    const toMatch = [
+      /Android/i,
+      /webOS/i,
+      /iPhone/i,
+      /iPad/i,
+      /iPod/i,
+      /BlackBerry/i,
+      /Windows Phone/i,
+    ];
+
+    return toMatch.some((toMatchItem) => {
+      return navigator.userAgent.match(toMatchItem);
+    });
+  }
 }
